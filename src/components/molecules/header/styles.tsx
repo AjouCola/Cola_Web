7f05355b79fedf58aecac3f17fa3f0c6316ba7e8
dmstmdrbs <dmstmdrbs98@gmail.com>
2022-05-22 17:58:58 +0900

    style: modify margin size of header

--- src/components/molecules/header/styles.tsx
+++ src/components/molecules/header/styles.tsx
@@ -60,6 +60,12 @@ const HeaderSection = styled.div`
   margin: 0 2rem;
   justify-content: space-around;
   align-items: center;
+  @media (max-width: ${({ theme: { breakpoints } }) => breakpoints.md}) {
+    margin: 0 1rem;
+  }
+  @media (max-width: ${({ theme: { breakpoints } }) => breakpoints.sm}) {
+    margin: 0 0.5rem;
+  }
 `;
 const HeaderBtn = styled.button`
   background: none;
