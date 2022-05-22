f8009ca0ed2da6dbdc9dc4ce2ccf1f75d0c95fa7
dmstmdrbs <dmstmdrbs98@gmail.com>
2022-05-22 22:01:18 +0900

    hotfix: add withCredential option in axios module

--- src/utils/api/core/index.ts
+++ src/utils/api/core/index.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const Api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || '',
+  withCredentials: true,
 });
 Api.defaults.timeout = 2500;
 Api.defaults.withCredentials = true;
