9553d4b4f0b9c496bd6d9b98ac15c5b4ce15adf3
coreight98 <maxcha98@naver.com>
2022-05-22 20:03:40 +0900

    fix : add credential true

--- src/utils/api/core/index.ts
+++ src/utils/api/core/index.ts
@@ -4,6 +4,7 @@ const Api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || '',
 });
 Api.defaults.timeout = 2500;
+Api.defaults.withCredentials = true;
 Api.interceptors.request.use(
   (config) => {
     return config;
