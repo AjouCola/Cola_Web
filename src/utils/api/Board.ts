135723dd8e2d3fa7ba903a4b942a92715d4a795c
dmstmdrbs <dmstmdrbs98@gmail.com>
2022-05-22 23:12:26 +0900

    feat: add Board api & create post method
    게시판 API 모듈 추가
    게시글 생성 API 적용

---
+++ src/utils/api/Board.ts
@@ -0,0 +1,16 @@
+import Api from './core';
+
+interface ICreatePost {
+  content: string;
+  title: string;
+}
+export const Board = {
+  async create({ content, title }: ICreatePost) {
+    return await Api.post('/api/v1/posts', { content, title }).catch((err) => console.error(err));
+  },
+  async get(postId: number) {
+    //
+    const { data }: any = await Api.get('/api/v1/posts/' + (postId + '')).catch((err) => console.log(err));
+    return data;
+  },
+};
