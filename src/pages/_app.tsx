62d973aae94d5cc3278cb69977959babd564f0ea
coreight98 <maxcha98@naver.com>
2022-05-24 01:14:41 +0900

    feat : add getUser logic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,8 +6,21 @@ import { RecoilRoot } from 'recoil';
 import Layout from '@components/Layout';
 import { theme } from '@styles/theme';
 import { getCookies } from '@utils/cookie';
-
+import { useCallback, useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import { userState } from '@store/user';
+import User from '@utils/api/User';
 function MyApp({ Component, pageProps }: AppProps) {
+  useCallback(async () => {
+    const session = getCookies('SESSION');
+    const [user, setUser] = useRecoilState(userState);
+    console.log(session);
+    if (session && user?.id === undefined) {
+      const data = (await User.getUserInfo()) as unknown as any;
+      setUser(data);
+    }
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <RecoilRoot>
