04933d54d316889c9a860271f114dbbc51d4e2e7
coreight98 <maxcha98@naver.com>
2022-05-24 01:45:33 +0900

    fix : change use function's position to modify life cycle error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,8 @@
 import '@styles/globals.css';
-import { useCallback, useEffect } from 'react';
 
 import { ThemeProvider } from '@emotion/react';
 import type { AppProps } from 'next/app';
-import { RecoilRoot, useRecoilState } from 'recoil';
+import { RecoilRoot } from 'recoil';
 
 import Layout from '@components/Layout';
 import { userState } from '@store/user';
@@ -12,17 +11,6 @@ import User from '@utils/api/User';
 import { getCookies } from '@utils/cookie';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [user, setUser] = useRecoilState(userState);
-  useCallback(async () => {
-    const session = getCookies('SESSION');
-
-    console.log(session);
-    if (session && user?.id === undefined) {
-      const data = (await User.getUserInfo()) as unknown as any;
-      setUser(data);
-    }
-  }, []);
-
   return (
     <ThemeProvider theme={theme}>
       <RecoilRoot>
