345cc46fa884a566792aebe723666e567656f99c
coreight98 <maxcha98@naver.com>
2022-05-24 01:42:13 +0900

    fix : change use function's position

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,21 @@
 import '@styles/globals.css';
+import { useCallback, useEffect } from 'react';
+
 import { ThemeProvider } from '@emotion/react';
 import type { AppProps } from 'next/app';
-import { RecoilRoot } from 'recoil';
+import { RecoilRoot, useRecoilState } from 'recoil';
 
 import Layout from '@components/Layout';
-import { theme } from '@styles/theme';
-import { getCookies } from '@utils/cookie';
-import { useCallback, useEffect } from 'react';
-import { useRecoilState } from 'recoil';
 import { userState } from '@store/user';
+import { theme } from '@styles/theme';
 import User from '@utils/api/User';
+import { getCookies } from '@utils/cookie';
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const [user, setUser] = useRecoilState(userState);
   useCallback(async () => {
     const session = getCookies('SESSION');
-    const [user, setUser] = useRecoilState(userState);
+
     console.log(session);
     if (session && user?.id === undefined) {
       const data = (await User.getUserInfo()) as unknown as any;
