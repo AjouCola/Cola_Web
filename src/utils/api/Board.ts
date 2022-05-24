48c07fb6c4f408b84c4cda875941c3c3ab8bb470
dmstmdrbs <dmstmdrbs98@gmail.com>
2022-05-24 23:37:31 +0900

    fix: change pageNumber of infinite query params

--- src/utils/api/Board.ts
+++ src/utils/api/Board.ts
@@ -22,8 +22,72 @@ export const Board = {
     })) as unknown as any;
     return {
       result: data.content,
-      nextPage: data.pageNumber + 1,
+      nextPage: data.pageable.pageNumber + 1,
       isLast: data.last,
     };
   },
 };
+
+// const data = {
+//   content: [
+//     {
+//       postId: 360,
+//       title: 'title19',
+//       userInfo: { userId: 23, userName: null },
+//       createdDate: '2022-05-24T05:46:24.349521',
+//       modifiedDate: '2022-05-24T05:46:24.349521',
+//     },
+//     {
+//       postId: 359,
+//       title: 'title18',
+//       userInfo: { userId: 23, userName: null },
+//       createdDate: '2022-05-24T05:46:24.336049',
+//       modifiedDate: '2022-05-24T05:46:24.336049',
+//     },
+//     {
+//       postId: 358,
+//       title: 'title17',
+//       userInfo: { userId: 23, userName: null },
+//       createdDate: '2022-05-24T05:46:24.322585',
+//       modifiedDate: '2022-05-24T05:46:24.322585',
+//     },
+//     {
+//       postId: 357,
+//       title: 'title16',
+//       userInfo: { userId: 23, userName: null },
+//       createdDate: '2022-05-24T05:46:24.309016',
+//       modifiedDate: '2022-05-24T05:46:24.309016',
+//     },
+//     {
+//       postId: 356,
+//       title: 'title15',
+//       userInfo: { userId: 23, userName: null },
+//       createdDate: '2022-05-24T05:46:24.29385',
+//       modifiedDate: '2022-05-24T05:46:24.29385',
+//     },
+//     {
+//       postId: 355,
+//       title: 'title14',
+//       userInfo: { userId: 23, userName: null },
+//       createdDate: '2022-05-24T05:46:24.280339',
+//       modifiedDate: '2022-05-24T05:46:24.280339',
+//     },
+//   ],
+//   pageable: {
+//     sort: { empty: false, sorted: true, unsorted: false },
+//     offset: 0,
+//     pageNumber: 0,
+//     pageSize: 6,
+//     paged: true,
+//     unpaged: false,
+//   },
+//   totalPages: 4,
+//   totalElements: 20,
+//   last: false,
+//   size: 6,
+//   number: 0,
+//   sort: { empty: false, sorted: true, unsorted: false },
+//   numberOfElements: 6,
+//   first: true,
+//   empty: false,
+// };
