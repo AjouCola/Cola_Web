fa1646f831b8f9ff493ff9093e7c65ab2991ba0e
dmstmdrbs <dmstmdrbs98@gmail.com>
2022-05-26 01:20:14 +0900

    feat: add Todo api & modify interface related with todo
    Todo API 연동
    관련된 타입 수정
    테스트를 위한 Drop 비활성화

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,20 @@
 import { atom, selector } from 'recoil';
 
+import { ITodoFolder } from '@utils/api/Todo';
 export interface IToDo {
   id: number;
   content: string;
+  createdAt: string;
+  modifiedAt?: string;
+  folderId?: number;
 }
 export interface ITodoState {
   [key: string]: IToDo[];
 }
-export const todoState = atom<ITodoState>({
+
+export const todoState = atom<ITodoFolder[]>({
   key: 'todoState',
-  default: {} as ITodoState,
+  default: [] as ITodoFolder[],
 });
 
 export const accessTokenState = atom<string>({
--- src/utils/api/Todo.ts
+++ src/utils/api/Todo.ts
@@ -5,32 +5,68 @@ import Api from './core';
 import { doings, toDos, dones } from '@constants/todoDummy';
 import { IToDo, ITodoState, todoState } from '@store/index';
 
-export const getTodoList = async (date: Date): Promise<ITodoState> => {
-  console.log('getTodolist', date);
-  const data: ITodoState = await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({
-        '할 일': toDos,
-        '하는 중': doings,
-        '다 함': dones,
-      } as ITodoState);
-    }, 1500);
-  });
-  console.log('todo data', data);
-  return data;
-};
+export interface IItemDto {
+  date: string;
+  folderId: number;
+  progress: number;
+  todos: string;
+}
 
-export const saveTodoList = async (date: Date, todoList: ITodoState): Promise<boolean> => {
-  // const res = await Api.post('/todo', {
-  //   date,
-  //   todoList: JSON.stringify(todoList),
-  // }).catch((err) => console.error(err));
+export interface IFolderDto {
+  color: string;
+  folderId: number;
+  name: string;
+}
 
-  // if (!res?.data?.success) return false;
-  console.log('save todolist', date, JSON.stringify(todoList));
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(true);
-    }, 1500);
-  });
+export interface ITodoFolder {
+  todos: any[];
+  color: string;
+  folderId: number;
+  name: string;
+  progress: number;
+}
+const TodoApi = {
+  getTodoList: async (date: Date): Promise<ITodoFolder[]> => {
+    const results: [IItemDto[], IFolderDto[]] = await Promise.all([
+      Api.get(`/api/v1/item/` + date.toISOString().slice(0, 10)) as unknown as IItemDto[],
+      Api.get('/api/v1/folder') as unknown as IFolderDto[],
+    ]);
+
+    const [ItemDtos, FolderDtos] = results;
+
+    const folderList = ItemDtos.map((itemDto, idx) => {
+      const parsedTodos = JSON.parse(itemDto.todos);
+
+      const folder = FolderDtos.find((folder) => folder.folderId === itemDto.folderId);
+
+      return {
+        todos: parsedTodos,
+        ...folder,
+        progress: itemDto.progress,
+      } as ITodoFolder;
+    });
+
+    return folderList;
+  },
+  saveTodoList: async (date: Date, todoList: ITodoFolder[]): Promise<boolean> => {
+    // const res = await Api.post('/todo', {
+    //   date,
+    //   todoList: JSON.stringify(todoList),
+    // }).catch((err) => console.error(err));
+
+    // if (!res?.data?.success) return false;
+    console.log('save todolist', date, JSON.stringify(todoList));
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(true);
+      }, 1500);
+    });
+  },
+  createFolder: async (name: string, color: string) => {
+    await Api.post('/api/v1/folder', {
+      name,
+      color,
+    });
+  },
 };
+export default TodoApi;
