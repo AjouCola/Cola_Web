91ad69236e600743f73bd7c170306b51f5b8d087
dmstmdrbs <dmstmdrbs98@gmail.com>
2022-05-27 13:23:58 +0900

    feat: add Comment delete API
    댓글 삭제 API 연동

--- src/utils/api/Comment.ts
+++ src/utils/api/Comment.ts
@@ -7,4 +7,9 @@ export const CommentApi = {
   async get(postId: number) {
     return await Api.post(`/api/v1/posts/${postId}/comments`);
   },
+  async delete(commentId: number) {
+    return await Api.delete(`/api/v1/comments/${commentId}`).catch(() =>
+      alert('댓글 삭제 중 문제가 발생하였습니다. 잠시 후 다시 시도해주세요.'),
+    );
+  },
 };
