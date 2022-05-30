c53f3b71e444e4e9d8aeef26c388882dbdc4c744
dmstmdrbs <dmstmdrbs98@gmail.com>
2022-05-30 16:49:35 +0900

    feat: add dummy data for home progress section

---
+++ src/constants/homeDummy.ts
@@ -0,0 +1,37 @@
+export const progressDummy = [
+  {
+    day: 'MON',
+    color: '#ff9ef0',
+    progress: 87,
+  },
+  {
+    day: 'TUE',
+    color: '#B7F3E2',
+    progress: 69,
+  },
+  {
+    day: 'WED',
+    color: '#6E9A02',
+    progress: 27,
+  },
+  {
+    day: 'THU',
+    color: '#23BAEF',
+    progress: 53,
+  },
+  {
+    day: 'FRI',
+    color: '#8C7AEE',
+    progress: 42,
+  },
+  {
+    day: 'SAT',
+    color: '#e4e70c',
+    progress: 10,
+  },
+  {
+    day: 'SUN',
+    color: '#ABE3ED',
+    progress: 68,
+  },
+];
