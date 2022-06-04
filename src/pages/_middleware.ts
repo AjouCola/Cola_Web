7fcfa57aee8ebfb3a0a761bcd1059a5cd30acccd
은승균 <97004643+TomEun@users.noreply.github.com>
2022-06-04 17:57:08 +0900

    feat: redirect to sign in page when logged out

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -6,9 +6,9 @@ export const middleware = async (req: NextRequest) => {
   const session = cookie.SESSION;
   const notLoginUrlList = ['/signIn', '/signUp', '/signUp/policy'];
 
-  // if (!session && !notLoginUrlList.some((path) => pathname.includes(path))) {
-  //   const url = req.nextUrl.clone();
-  //   url.pathname = '/signIn';
-  //   return NextResponse.rewrite(url);
-  // }
+  if (!session && !notLoginUrlList.some((path) => pathname.includes(path))) {
+    const url = req.nextUrl.clone();
+    url.pathname = '/signIn';
+    return NextResponse.rewrite(url);
+  }
 };
