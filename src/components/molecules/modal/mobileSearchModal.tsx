3e2ef194a2d2d85867925cade521763ea4cbf698
dmstmdrbs <dmstmdrbs98@gmail.com>
2022-06-08 00:45:16 +0900

    feat: add mobile search modal
    모바일 화면에서 검색 모달 추가
    검색 로직 재사용을 위한 분리 필요

--- src/components/molecules/modal/mobileSearchModal.tsx
+++ src/components/molecules/modal/mobileSearchModal.tsx
@@ -1,9 +1,15 @@
-import styled from '@emotion/styled';
+import { DetailedHTMLProps, InputHTMLAttributes, useRef, useState } from 'react';
 
-import { HeaderBtn } from '../searchBar/styles';
+import styled from '@emotion/styled';
+import { useRouter } from 'next/router';
+
+import { HashtagWrapper, HistoryItem, HistoryWrapper, HistoryControlWrapper } from '../searchBar/styles';
 
 import SearchIcon from '@assets/icon/search.svg';
 import Input from '@components/atoms/input';
+import SearchHistoryItem from '@components/atoms/searchHistoryItem';
+import { dateFormatYYYYmmDD } from '@utils/libs/formatDate';
+import useSearchHistory from '@utils/libs/useSearchHistory';
 
 const Container = styled.div`
   position: fixed;
@@ -13,38 +19,151 @@ const Container = styled.div`
   height: 100vh;
   z-index: 9999;
   background: white;
-  padding: 1rem;
+  padding: 0.5rem;
+  display: flex;
+  flex-direction: column;
 
   @media (min-width: ${({ theme: { breakpoints } }) => breakpoints.md}) {
     display: none;
   }
 `;
+
+const CloseBtnWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  height: 3rem;
+`;
+const CloseBtn = styled.div`
+  width: 3rem;
+  height: 3rem;
+  position: relative;
+  span::after,
+  span::before {
+    position: absolute;
+    top: calc(50% - 1px);
+    left: calc(50% - 15px);
+    content: ' ';
+    width: 30px;
+    height: 2px;
+    background-color: #666;
+  }
+  span::after {
+    transform: rotate(45deg);
+  }
+  span::before {
+    transform: rotate(-45deg);
+  }
+`;
+
+const InputWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  gap: 1rem;
+  justify-content: space-between;
+  align-items: center;
+`;
+const HeaderBtn = styled.button`
+  background: none;
+  border: none;
+  font-size: 14px;
+  color: whitesmoke;
+  cursor: pointer;
+`;
+const SearchHistoryWrapper = styled.div`
+  flex: 1;
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    width: 9px;
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background: ${({ theme }) => theme.colors.blue[500]};
+  }
+`;
+
 const MSearchModal = ({ onClickMSearch }: { onClickMSearch: () => void }) => {
-  const handleSubmit = () => {
-    //
+  const router = useRouter();
+  const inputRef = useRef<HTMLInputElement>();
+  const [chipList, setChipList] = useState<string[]>([]);
+  const [searchHistory, setSearchHistory] = useSearchHistory();
+
+  const onClickDelete = (id: number) => {
+    setSearchHistory((prev) => {
+      const newHistory = [...prev];
+      const historyIndex = newHistory.findIndex((v) => v.id === id);
+
+      newHistory.splice(historyIndex, 1);
+
+      localStorage.setItem('cola-search-history', JSON.stringify(newHistory));
+      return newHistory;
+    });
+  };
+
+  const addChipList = (event: DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>) => {
+    if (event.key !== ',' || inputRef.current === undefined) return;
+    setChipList([...chipList, inputRef.current.value.split(',')[0]]);
+    inputRef.current.value = '';
+  };
+
+  const handleSubmit = async () => {
+    if (!inputRef.current) return;
+
+    const currentHistory = JSON.parse(localStorage.getItem('cola-search-history') ?? '[]');
+    if (Array.isArray(currentHistory)) {
+      const searchKeywordItem = {
+        id: Date.now(),
+        keyword: inputRef.current?.value,
+        date: dateFormatYYYYmmDD(new Date()),
+      };
+      const newHistory = [searchKeywordItem, ...currentHistory];
+      localStorage.setItem('cola-search-history', JSON.stringify(newHistory));
+      setSearchHistory((prev) => [searchKeywordItem, ...prev]);
+      inputRef.current.value = '';
+
+      router.push({
+        pathname: '/board/search',
+        query: {
+          keyword: searchKeywordItem.keyword,
+        },
+      });
+    }
   };
   return (
     <Container>
-      <div style={{ width: '100%', padding: '0 1rem', height: '2rem' }}>
-        <button style={{ float: 'right' }} onClick={onClickMSearch}>
+      <CloseBtnWrapper>
+        {/* <button style={{ float: 'right' }} onClick={onClickMSearch}>
           X
-        </button>
-      </div>
-      <div
-        style={{
-          position: 'relative',
-          width: '100%',
-          display: 'flex',
-          gap: '1rem',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-      >
-        <Input width="80%" />
+        </button> */}
+        <CloseBtn onClick={onClickMSearch}>
+          <span></span>
+        </CloseBtn>
+      </CloseBtnWrapper>
+      <InputWrapper style={{}}>
+        <Input
+          width="85%"
+          height="3rem"
+          ref={(el) => (inputRef.current = el as HTMLInputElement)}
+          onKeyUp={addChipList}
+          onKeyPress={(e) => {
+            if (e.key === 'Enter') {
+              handleSubmit();
+            }
+          }}
+        />
         <HeaderBtn type="button" onClick={handleSubmit}>
           <SearchIcon />
         </HeaderBtn>
-      </div>
+      </InputWrapper>
+      <SearchHistoryWrapper>
+        <HistoryWrapper>
+          {searchHistory.map((history) => (
+            <SearchHistoryItem key={history.id} history={history} onClickDelete={onClickDelete} />
+          ))}
+        </HistoryWrapper>
+      </SearchHistoryWrapper>
     </Container>
   );
 };
