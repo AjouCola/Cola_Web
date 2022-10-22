7dbf98fac2f98334a5442b207329ea8363e42196
은승균 <eunseung-gyun@eunseung-gyun-ui-Macmini.local>
2022-10-23 02:10:55 +0900

    refactor: modify todoArea

--- src/components/molecules/todoArea/index.tsx
+++ src/components/molecules/todoArea/index.tsx
@@ -1,17 +1,15 @@
-import { useRef, useState, useEffect } from 'react';
+import { ReactNode, useRef, useState } from 'react';
 
-import { GetServerSideProps } from 'next';
-import { resetServerContext } from 'react-beautiful-dnd';
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { useRecoilState, useSetRecoilState } from 'recoil';
 
 import { Container, Wrapper, FolderTitleWrapper, BtnAddTodo } from './styles';
 
 import FolderIcon from '@atoms/FolderIcon';
 import DraggableTodo from '@components/atoms/todoCheckBox/draggable';
 import TodoCheckBox from '@components/atoms/todoCheckBox/index';
-import { todoEditMode, todoModalContent } from '@store/todo';
-import TodoApi, { IFolder, ITodo } from '@utils/api/Todo';
-import { ITodoFolder, todoListState, todoState } from 'src/store';
+import { todoModalContent } from '@store/todo';
+import TodoApi, { ITodo } from '@utils/api/Todo';
+import { ITodoFolder, todoListState } from 'src/store';
 
 export interface ITodoAreaProps {
   date: string;
@@ -23,10 +21,7 @@ export interface ITodoAreaProps {
   dragMode?: boolean;
   deleteMode: boolean;
   checkDelete: (todoAre: number, todoId: number) => void;
-  children: any;
-}
-interface Props {
-  [key: string]: string[];
+  children: ReactNode;
 }
 
 const TodoArea = ({
@@ -35,48 +30,20 @@ const TodoArea = ({
   area,
   areaId,
   folderColor,
-  idx,
   dragMode = false,
   deleteMode,
   checkDelete,
   children,
 }: ITodoAreaProps) => {
-  const [todoList, setTodoList] = useRecoilState(todoListState);
-
-  const [focus, setFocus] = useState(false);
-  // const [todo, setTodoList] = useState<Props>({});
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  const [editMode, setEditMode] = useRecoilState(todoEditMode);
+  const setTodoList = useSetRecoilState(todoListState);
   const [editValue, setEditValue] = useRecoilState(todoModalContent);
 
-  const handleClick = (key: number) => {
-    console.log('handleClick', key, todoList);
-    const newToDo: ITodo = {
-      id: Date.now(),
-      content: '',
-      status: 'todo',
-    };
-    setTodoList((currentFolders) => {
-      console.log('currentFolders, key', currentFolders, key);
-      const currentFolderIndex = currentFolders.findIndex((v) => v.folder_id === key);
-      console.log('currentFolderIndex', currentFolderIndex);
-      const currentFolder = JSON.parse(JSON.stringify(currentFolders[currentFolderIndex])) as ITodoFolder;
-      currentFolder.todos.push(newToDo);
+  const [focus, setFocus] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
 
-      const newFolders: ITodoFolder[] = [...currentFolders];
-      newFolders[currentFolderIndex] = currentFolder;
-
-      return newFolders;
-    });
-    setFocus(true);
-  };
   const handleFocus = async (key: number, value: string, todoId?: number) => {
     setFocus(false);
-    console.log('handleFocus', key, value, todoId, editValue.id);
     if (editValue.id) {
-      // 수정 모드
-      console.log('수정모드');
       const modified: ITodo = {
         id: Date.now(),
         content: value + '',
@@ -140,6 +107,32 @@ const TodoArea = ({
     }
   };
 
+  const addTodo = (key: number) => {
+    const newToDo: ITodo = {
+      id: Date.now(),
+      content: '',
+      status: 'todo',
+    };
+    setTodoList((currentFolders) => {
+      const currentFolderIndex = currentFolders.findIndex((v) => v.folder_id === key);
+      const currentFolder = JSON.parse(JSON.stringify(currentFolders[currentFolderIndex])) as ITodoFolder;
+      currentFolder.todos.push(newToDo);
+
+      const newFolders: ITodoFolder[] = [...currentFolders];
+      newFolders[currentFolderIndex] = currentFolder;
+
+      return newFolders;
+    });
+    setFocus(true);
+  };
+
+  const handleAddTodo = () => {
+    if (focus) {
+      return;
+    }
+    addTodo(areaId);
+  };
+
   return (
     <Container>
       <FolderTitleWrapper>
@@ -149,46 +142,25 @@ const TodoArea = ({
           </span>
           <span>{area}</span>
         </div>
-        <BtnAddTodo onClick={() => !focus && handleClick(areaId)}>+</BtnAddTodo>
+        <BtnAddTodo onClick={handleAddTodo}>+</BtnAddTodo>
       </FolderTitleWrapper>
       <Wrapper>
-        {
-          // dragMode에 따라 드래그 가능한 컴포넌트 or 일반 컴포넌트 렌더링
-
-          todoItems?.map(({ id, content, status }, index) =>
-            dragMode ? (
-              <DraggableTodo
-                key={id}
-                toDoId={id}
-                toDoContent={content}
-                toDoStatus={status}
-                target={area}
-                targetId={areaId}
-                handleFocus={handleFocus}
-                inputRef={inputRef}
-                index={index}
-                deleteMode={deleteMode}
-                checkDelete={checkDelete}
-                date={date}
-              />
-            ) : (
-              <TodoCheckBox
-                key={id}
-                toDoId={id}
-                toDoContent={content}
-                toDoStatus={status}
-                target={area}
-                targetId={areaId}
-                handleFocus={handleFocus}
-                inputRef={inputRef}
-                index={index}
-                deleteMode={deleteMode}
-                checkDelete={checkDelete}
-                date={date}
-              />
-            ),
-          )
-        }
+        {todoItems?.map(({ id, content, status }, index) => {
+          const props = {
+            toDoId: id,
+            toDoContent: content,
+            toDoStatus: status,
+            target: area,
+            targetId: areaId,
+            handleFocus: handleFocus,
+            inputRef: inputRef,
+            index: index,
+            deleteMode: deleteMode,
+            checkDelete: checkDelete,
+            date: date,
+          };
+          return dragMode ? <DraggableTodo key={id} {...props} /> : <TodoCheckBox key={id} {...props} />;
+        })}
         {dragMode && children}
       </Wrapper>
     </Container>
